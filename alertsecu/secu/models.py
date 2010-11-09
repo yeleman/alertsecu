a3d787fa92020501c81f9c720d03e3caaa6d2f23
Alou Dolo <aloud7@gmail.com>
2010-11-09 08:58:42 +0000

    correction du model

--- alertsecu/secu/models.py
+++ alertsecu/secu/models.py
@@ -7,34 +7,41 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Person(User):
+    """ personne a enregistre """
     phone = models.CharField(max_length=30, blank=True,
     verbose_name=("Telephone"))
     num_passport = models.CharField(max_length=30,
                                     verbose_name=("Numero passport"))
     def __unicode__(self):
-        return _(u'%(name)s') % {"name" : self.username}
+        return (u'%(name)s') % {"name" : self.username}
 
 class Region (models.Model):
+    """ Region concernee """
     name = models.CharField(max_length=15, verbose_name=("Region"))
     code = models.CharField(max_length=5, blank=True,
                             verbose_name=("Code Region"))
     def __unicode__(self):
-        return _(u"%(name)s : (code)s") % {'name': self.name,
-                                           'code': self.code}
+        return (u"%(name)s : %(code)s") % {'name': self.name,
+                                          'code': self.code}
 
 class Alert(models.Model):
+    """ alerte """
     region = models.ForeignKey(Region,
                                 verbose_name=("Region"),
                                 related_name='regions')
     type = models.CharField(max_length=15, verbose_name=("Alerte"))
     def __unicode__(self):
-        return _(u"%(type)s : (code)s") % {'name': self.type}
+        return (u"%(type)s : %(region)s") % {'type': self.type,
+                                            'region' : self.region}
 
 class Messages(models.Model):
+    """ message a envoie """
     type = models.ForeignKey(Alert,
                              verbose_name=("Type"), related_name='types')
     msg = models.TextField(verbose_name=("Libellé"))
     date = models.DateField(verbose_name=("Date"),
                             default=datetime.datetime.today)
 
-
+    def __unicode__(self):
+        return (u"%(type)s : %(date)s") % {'type': self.type,
+                                            'date' : self.date}
