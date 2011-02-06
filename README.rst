fef00b8c038544ac7e0358872975341100fe6a1b
ksamuel <info.ksamuel@gmail.com>
2011-02-06 16:18:08 +0100

    Added AJAX effects and autoreload to simulation

--- README.rst
+++ README.rst
@@ -1,4 +1,28 @@
-Dependances:
-	Django;
-	South
+Setup
+======
+
+Install dependances:
+
+* Django 1.2+ (pip install django);
+* Logger-ng, branch 'new-core' (git@github.com:ksamuel/Logger-NG.git)
+* Bolibona, branch 'who' (git@github.com:bolibana/bolibana.git)
+* Direct-sms, branch 'new-core' (git@github.com:ksamuel/Direct-SMS.git)
+* django_simple_config (git://github.com/yeleman/django_simple_config.git)
+* djtable (git://github.com/adammck/djtables.git)
+* code_generator (git@github.com:yeleman/code_generator.git)
+* djappsettings (git://github.com/adammck/djappsettings.git)
+* json (native in Python 2.6) or simplejson (pip install simplejson)
+
+Create database:
+
+./manage.py syncdb
+
+Run dev servers:
+
+./manage.py runserver
+./manage.py runrouter
+
+Send SMS using address:
+
+http://127.0.0.1:8000/httptester/
 
--- alertsecu/secu/views.py
+++ alertsecu/secu/views.py
@@ -3,12 +3,20 @@
 
 import datetime
 
-from django.shortcuts import render_to_response, HttpResponseRedirect
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+from django.shortcuts import (render_to_response, HttpResponseRedirect, 
+                              HttpResponse)
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.template import RequestContext
 from django.contrib import messages
 from django.contrib.messages.api import get_messages
+
+from django.core import serializers
 
 from secu.models import Visitor, Area, Visit
 from secu.forms import AreaForm
@@ -16,7 +24,6 @@ from secu.forms import AreaForm
 from direct_sms.utils import send_msg
 
 def simulation(request):
-
     
     messages = tuple(iter(get_messages(request)))
     # todo: make this with tags
@@ -27,7 +34,14 @@ def simulation(request):
     
     visits = Visit.objects.filter(departure_date__gte=datetime.date.today())
     visitors = [visit.visitor for visit in visits]
-    
+
+    # if this is for an ajax call, reformat for json output
+    if 'application/json' in request.META.get('HTTP_ACCEPT', ''):
+        visitors = json.loads(serializers.serialize('json', visitors))
+        visitors = ((v['fields']["passport_number"], v['fields']) for v in visitors)
+        return HttpResponse(json.dumps(dict(visitors)),
+                            mimetype='application/json')
+
     return render_to_response('simulation.html', locals(),
                               context_instance=RequestContext(request))
     
